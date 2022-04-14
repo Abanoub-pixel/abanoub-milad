2dd65eefc9d5359b22cbcaa6a34bb872156005c0
Abanoub  Milad <abanoubmilad04@gmail.com>
2022-04-15 01:17:40 +0200

    Delete unused css files

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,12 +3,10 @@ import { Route, Redirect } from "react-router-dom";
 import Details from "./section/Details";
 import Cart from "./section/Cart";
 import Products from "./section/Products";
-import "./css/section.css";
 export class Section extends Component {
   render() {
     return (
       <section>
-        {/* <Route component={Products} /> */}
         <Route path="/products/:cat" component={Products} exact />
         <Route path="/product/:id" component={Details} />
         <Route path="/cart/:id" component={Cart} />
