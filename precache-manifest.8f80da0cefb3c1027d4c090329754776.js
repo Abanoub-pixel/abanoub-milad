c570f8e8b7b2dc3b6dceeb21855cbd0486a0064f
Abanoub  Milad <abanoubmilad04@gmail.com>
2022-04-15 01:16:21 +0200

    Updates

---
+++ precache-manifest.8f80da0cefb3c1027d4c090329754776.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d4ba2dda18bcd77dbff799ab8ea0aa",
+    "url": "/abanoub-milad/index.html"
+  },
+  {
+    "revision": "a7e0bf7e6782b34d4e3d",
+    "url": "/abanoub-milad/static/css/main.7059c2d7.chunk.css"
+  },
+  {
+    "revision": "2c69871dda14868d3545",
+    "url": "/abanoub-milad/static/js/2.0553cfd9.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/abanoub-milad/static/js/2.0553cfd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0c55b11389cad6346f",
+    "url": "/abanoub-milad/static/js/3.fbc1d7f7.chunk.js"
+  },
+  {
+    "revision": "a7e0bf7e6782b34d4e3d",
+    "url": "/abanoub-milad/static/js/main.08f5b30a.chunk.js"
+  },
+  {
+    "revision": "59193e055ec77753a94f",
+    "url": "/abanoub-milad/static/js/runtime-main.daec7a6e.js"
+  },
+  {
+    "revision": "3186ed92d18f45a0e38da21443b87ed7",
+    "url": "/abanoub-milad/static/media/bars-solid.3186ed92.svg"
+  },
+  {
+    "revision": "682015d22ed861d4efc5cf6de431d346",
+    "url": "/abanoub-milad/static/media/xmark-solid.682015d2.svg"
+  }
+]);
